edb61eba011a60c788302cf47ed11dbcd1141b3e
Abdulsattar Alkhalaf <AboAlimk@gmail.com>
2024-06-04 02:55:49 +0300

    Update

--- A3 Framework/A3/A3App/A3DataBase/A3DataBaseQuery.php
+++ A3 Framework/A3/A3App/A3DataBase/A3DataBaseQuery.php
@@ -16,7 +16,7 @@ class A3DataBaseQuery
 
     public static function query($connection, $statement): bool|array
     {
-        $result = $connection->getConnection()->query($statement);
+        $result = self::executeQuery($connection, $statement);
         if (is_object($result) && get_class($result) === 'mysqli_result') {
             return $result->fetch_all(MYSQLI_ASSOC);
         } else if (is_bool($result)) {
@@ -44,21 +44,28 @@ class A3DataBaseQuery
 
     public static function multiQuery($connection, $statement, $getLastId)
     {
+        if(is_bool($statement)){
+            return false;
+        }
+        if(is_string($statement)){
+            $statement = [$statement];
+        }
         $ids = [];
-        $result = $connection->getConnection()->multi_query($statement);
-        if ($result) {
-            do {
-                if ($connection->getConnection()->insert_id) {
-                    $ids[] = $connection->getConnection()->insert_id;
-                }
-                if ($connection->getConnection()->more_results()) {
-                    if ($_result = $connection->getConnection()->store_result()) {
-                        $_result->free();
-                    }
-                }
-            } while ($connection->getConnection()->next_result());
+        $result = false;
+        foreach($statement as $st){
+            $result = self::query($connection, $st);
+            if($result === true && $connection->getConnection()->insert_id){
+                $ids[] = $connection->getConnection()->insert_id;
+            }
         }
         return $getLastId ? $ids : $result;
     }
 
+    private static function executeQuery($connection, $statement){
+        if (version_compare(phpversion(), '8.2', '>=')) {
+            return $connection->getConnection()->execute_query($statement);
+        } else {
+            return $connection->getConnection()->query($statement);
+        }
+    }
 }
